d26e85add044fc22bf47f6e9f728d96f3e9b49f8
Sleenma <quangd368@gmail.com>
2024-11-12 06:35:51 +0700

    fill du lieu thong ke

--- src/redux/reducer/BillDetailReducer.js
+++ src/redux/reducer/BillDetailReducer.js
@@ -4,7 +4,10 @@ import {
     FETCH_BILL_DETAILS_FAILURE,
     UPDATE_BILL_STATUS,
     COMPLETE_BILL,
-    DELETE_PRODUCT
+    DELETE_PRODUCT,
+    FETCH_BILL_STATISTICS_REQUEST,
+    FETCH_BILL_STATISTICS_SUCCESS,
+    FETCH_BILL_STATISTICS_FAILURE
 } from './BillDetailTypes';
 
 const initialState = {
@@ -13,8 +16,10 @@ const initialState = {
     billSummary: null,
     totalPages: 0,
     loading: false,
+    statisticsLoading: false, // Separate loading state for statistics
     error: null,
-    status: { status1: false, status2: false, status3: false, status4: false }
+    status: { status1: false, status2: false, status3: false, status4: false },
+    billDetailStatistics: [] // New state for holding statistics data
 };
 
 export const billDetailReducer = (state = initialState, action) => {
@@ -38,6 +43,12 @@ export const billDetailReducer = (state = initialState, action) => {
             return { ...state, billSummary: { ...state.billSummary, status: 'COMPLETED' } };
         case DELETE_PRODUCT:
             return { ...state, billDetail: state.billDetail.filter(product => product.productCode !== action.payload) };
+        case FETCH_BILL_STATISTICS_REQUEST:
+            return { ...state, statisticsLoading: true, error: null };
+        case FETCH_BILL_STATISTICS_SUCCESS:
+            return { ...state, billDetailStatistics: action.payload, statisticsLoading: false };
+        case FETCH_BILL_STATISTICS_FAILURE:
+            return { ...state, statisticsLoading: false, error: action.payload };
         default:
             return state;
     }
